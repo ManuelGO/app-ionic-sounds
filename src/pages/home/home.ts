e73d2e84f73830e7957b326b185e4aa2b14c80cb
Manuel Gonzalez <manuelarvelo19@gmail.com>
2017-10-17 00:38:16 -0400

    Aplicación finalizada.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,63 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ANIMALES } from '../../data/data.animales';
+import { Animal } from '../../interfaces/animal.interface';
+import { Refresher, reorderArray } from 'ionic-angular';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  animales:Animal[] = [];
+  audio = new Audio();
+  audioTiempo: any;
+  ordenando: boolean = false;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor() {
+    this.animales = ANIMALES.slice(0);
   }
 
+  reproducir(animal:Animal){
+    this.pausar_audio(animal);
+
+    if( animal.reproduciendo){
+      animal.reproduciendo = false;
+      return;
+    }
+
+    this.audio.src = animal.audio;
+    animal.reproduciendo = true;
+    this.audio.load();
+    this.audio.play();
+    this.audioTiempo = setTimeout(()=> animal.reproduciendo =  false, animal.duracion * 1000)
+  }
+
+  private pausar_audio(animalSel: Animal){
+    clearTimeout(this.audioTiempo);
+    this.audio.pause();
+    this.audio.currentTime = 0;
+    for (let animal of this.animales){
+      if (animal.nombre != animalSel.nombre){
+        animal.reproduciendo = false;
+      }
+    }
+  }
+
+  borrarAnimal(idx:number){
+    this.animales.splice(idx, 1);
+  }
+
+recargarAnimales(refresher:Refresher){
+  console.log("Inicio del refresh");
+  setTimeout(()=>{
+    console.log('fin');
+    this.animales = ANIMALES.slice(0);
+    refresher.complete()
+  }, 1500);
+}
+reordenarAnimales(indices:any){
+  this.animales = reorderArray(this.animales, indices);
+}
 }
